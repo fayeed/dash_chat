028dec0957d97f4bd3bab82c6fdb4cf97bdb685e
Fayeed <fayeed@live.com>
2020-06-08 11:46:24 +0530

    Readme.md updated & ScrollToBottom widget documented

--- lib/src/models/scroll_to_bottom_style.dart
+++ lib/src/models/scroll_to_bottom_style.dart
@@ -1,14 +1,49 @@
 part of dash_chat;
 
 class ScrollToBottomStyle {
+  /// Background color of the scrollToBottom widget
+  ///
+  /// Defaults to theme accent color
   Color backgroundColor;
+
+  /// Text/ Icon color for scrollToBottom widget
+  ///
+  /// Defaults to white
   Color textColor;
+
+  /// Height of the scrollToBottom widget
+  ///
+  /// Defaults to `45.0`
   double height;
+
+  /// Width of the scrollToBottom widget
+  ///
+  /// Defaults to `45.0`
   double width;
+
+  /// Top absolute position of the widget
+  ///
+  /// Defaults to 0.0
   double top;
+
+  /// Left absolute position of the widget
+  ///
+  /// Defaults to 0.0
   double left;
+
+  /// Right absolute position of the widget
+  ///
+  /// Defaults to 20.0
   double right;
+
+  /// Bottom absolute position of the widget
+  ///
+  /// Defaults to 60.0
   double bottom;
+
+  /// Icon inside the widget
+  ///
+  /// Defaults to material icon -> `keyboard_arrow_down`
   IconData icon;
 
   ScrollToBottomStyle({
