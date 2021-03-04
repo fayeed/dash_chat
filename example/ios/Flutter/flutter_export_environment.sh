e463ef4ed2cecd9355aad623fcd21c688cf0fc30
diego <dgarcia@triploop.com>
2021-03-04 10:21:53 -0700

    migrated to null safety

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/diego/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/diego/Documents/HomeTask/dash_chat/example"
+export "FLUTTER_TARGET=/Users/diego/Documents/HomeTask/dash_chat/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/diego/Documents/HomeTask/dash_chat/example/.dart_tool/package_config.json"
